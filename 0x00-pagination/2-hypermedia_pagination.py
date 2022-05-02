3a2c89d6d53f745708d07eaba0f9ad9e7eb77bc3
Cepheus-king <84628709+cepheus-king@users.noreply.github.com>
2022-05-02 15:04:25 +0100

    hypermedia_pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -52,4 +52,4 @@ class Server:
 
 def index_range(page: int, page_size: int) -> tuple:
     """ return [page size] """
-    return ((page - 1) * page_size, page * page_size
+    return ((page - 1) * page_size, page * page_size)
