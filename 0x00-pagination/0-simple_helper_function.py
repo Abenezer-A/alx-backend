2a459dae761fa47a451f25c28806c076b7f6a8df
Cepheus-king <84628709+cepheus-king@users.noreply.github.com>
2022-05-04 19:20:53 +0100

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,3 +7,4 @@ def index_range(page: int, page_size: int) -> tuple:
     mytuple = ((page - 1) * page_size, page * page_size)
     """ assigning mytuple"""
     return mytuple 
+    """return mytuple"""
